dd72160c7afc3c108ed0aa0a201b6aa23e22f271
AlexaKosakova <34619765+AlexaKosakova@users.noreply.github.com>
2019-03-03 13:36:51 +0300

    работающий вариант со статусами

--- Async.js
+++ Async.js
@@ -2,89 +2,137 @@ var btn;
 var input;
 var error;
 var outputs;
+var statuses = [];
+const WAIT = 0;
+const SUCCESS = 1;
+const ERROR = 2;
 
 function loadPage() {
   btn = document.getElementById("btn");
   input = document.getElementById("input");
   error = document.getElementById("error");
-  outputs = [document.getElementById("output1"),document.getElementById("output2")];
-  btn.addEventListener("click", function(event){
+  outputs = [document.getElementById("output1"), document.getElementById("output2")];
+  for (i = 0; i < outputs.length; i++) {
+    statuses.push("status");
+  };
+  btn.addEventListener("click", function (event) {
     outputs[0].innerHTML = "";
     outputs[1].innerHTML = "";
     error.innerHTML = "";
-    if (ckeck()){
-      asyncFunction(input.value).onSuccess(successCallback).onError(errorCallback).rise();
-    }  
+    ckeck();
+    asyncFunction(input.value).onSuccess(successCallback).onError(errorCallback);
   });
 }
 
 var objError = {
-  1: {"код": 418, "сообщение": "я — чайник"},
-  2: {"код": 504, "сообщение": "шлюз не отвечает"},
-  3: {"код": 408, "сообщение": "истекло время ожидания"},
-  4: {"код": 407, "сообщение": "необходима аутентификация прокси"},
-  5: {"код": 422, "сообщение": "необрабатываемый экземпляр"},
-  6: {"код": 451, "сообщение": "недоступно по юридическим причинам"},
-  7: {"код": 506, "сообщение": "вариант тоже проводит согласование"},
-  8: {"код": 520, "сообщение": "неизвестная ошибка"},
-  9: {"код": 509, "сообщение": "исчерпана пропускная ширина канала"},
-  10: {"код": 203, "сообщение": "информация не авторитетна"},
+  1: {
+    "код": 418,
+    "сообщение": "я — чайник"
+  },
+  2: {
+    "код": 504,
+    "сообщение": "шлюз не отвечает"
+  },
+  3: {
+    "код": 408,
+    "сообщение": "истекло время ожидания"
+  },
+  4: {
+    "код": 407,
+    "сообщение": "необходима аутентификация прокси"
+  },
+  5: {
+    "код": 422,
+    "сообщение": "необрабатываемый экземпляр"
+  },
+  6: {
+    "код": 451,
+    "сообщение": "недоступно по юридическим причинам"
+  },
+  7: {
+    "код": 506,
+    "сообщение": "вариант тоже проводит согласование"
+  },
+  8: {
+    "код": 520,
+    "сообщение": "неизвестная ошибка"
+  },
+  9: {
+    "код": 509,
+    "сообщение": "исчерпана пропускная ширина канала"
+  },
+  10: {
+    "код": 203,
+    "сообщение": "информация не авторитетна"
+  },
 };
 
-function ckeck(){
+function ckeck() {
   input.style.backgroundColor = "";
   error.innerHTML = "";
   if (isNaN(+input.value.replace(",", "."))) {
-     input.style.backgroundColor = "red";
-     error.innerHTML = "Number requred";
-     return false;
-    }
-  return true;
+    input.style.backgroundColor = "red";
+    error.innerHTML = "Number requred";
+  }
 };
-  
-function successCallback(n, output){
+
+function successCallback(n, j) {
   n *= 10;
-  output.innerHTML = n;
+  outputs[j].innerHTML = n;
   return n;
 }; //calculation
 
-function errorCallback(n){
+function errorCallback() {
   var r = (Math.floor(Math.random() * (10)) + 1);
-  error.innerHTML = objError[r]["код"]+"<br/>"+objError[r]["сообщение"];
+  error.innerHTML = objError[r]["код"] + "<br/>" + objError[r]["сообщение"];
 }; //error
 
 function asyncFunction(n) {
-  this.onSuccessCallback = null;
-  this.onSuccess = function(callback){
-      onSuccessCallback = callback;
-      return this;
-  }, 
-  this.onErrorCallback = null;
-  this.onError = function(callback){
-      onErrorCallback = callback;
-      return this;
+  for (i = 0; i < statuses.length; i++) {
+    statuses[i] = WAIT;
+    console.log(i);
+  };
+
+  /*var timer = setTimeout(function () {*/
+  for (i = 0; i < statuses.length; i++) {
+    console.log(i + 10);
+    if (Math.random() < 0.7) {
+      statuses[i] = SUCCESS;
+    } else {
+      statuses[i] = ERROR;
+      break;
+    }
+
   }
-  this.rise =  function(i) {
-    i = i ? i : 0; //если i задано, то i, если нет, то 0
-    if (i<outputs.length){
-      if (Math.random() < 0.7) {
-        n=onSuccessCallback(n, outputs[i]);
-        rise(i+1);
-      } else {
-        onErrorCallback(n);
-      } 
+  /*}, 1000);*/
+
+  //(Math.floor(Math.random() * (3)) + 3));
+
+  var onObj = {
+    onSuccessCallback: null,
+    onSuccess: function (callback) {
+      var timer = setTimeout(function () {
+        for (i = 0; i < statuses.length; i++) {
+          if (statuses[i] === SUCCESS) {
+            n = callback(n, i);
+            console.log("Succes" + i);
+          }
+        };
+      }, Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000);
+      return this;
+    },
+    onErrorCallback: null,
+    onError: function (callback) {
+      var timer = setTimeout(function () {
+        for (i = 0; i < statuses.length; i++) {
+          if (statuses[i] === ERROR) {
+            callback();
+          }
+        };
+
+      }, Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000);
+      return this;
     }
   }
-
-  return this;
-};
-
-/*var odd = Math.random();
-    if (odd < 0.7) {
-      done
-    } else {
-      error
-    } 
-*/
-
-
+  return onObj;
+}
